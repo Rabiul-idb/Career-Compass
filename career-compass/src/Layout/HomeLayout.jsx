8bfb9e38c3af445d6549cf3c5f6d2d621498488a
rabiul-idb <rabiul.idb40@gmail.com>
2024-11-18 15:42:23 +0600

    template ready

---
+++ career-compass/src/Layout/HomeLayout.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Components/Navbar";
+
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h1>this is main layout</h1>
+        </div>
+    );
+};
+
+export default HomeLayout;
---
+++ career-compass/src/Layout/MainLayout.jsx
@@ -0,0 +1,13 @@
+
+import HomeLayout from "./HomeLayout";
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            <HomeLayout></HomeLayout>
+        </div>
+    );
+};
+
+export default MainLayout;
